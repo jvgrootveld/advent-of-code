6158bec2098dbbf78261ae1c47d9fdfc16825867
Justin van Grootveld <justin.vangrootveld@gmail.com>
2022-12-01 09:44:20 +0100

    Add 2022 day 1

--- 2022/src/main/kotlin/day01/Day01.kt
+++ 2022/src/main/kotlin/day01/Day01.kt
@@ -5,11 +5,45 @@ class Day01 {
     companion object {
     
         fun part1(input: List<String>): Int {
-            TODO("IMPLEMENT")
+
+            var currentCalories = 0
+            var highestCalories = 0
+
+            for (line in input) {
+                if (line.isEmpty()) {
+                    // Set and reset
+                    highestCalories = highestCalories.coerceAtLeast(currentCalories);
+                    currentCalories = 0
+                } else {
+                    currentCalories += line.toInt()
+                }
+            }
+
+            return highestCalories.coerceAtLeast(currentCalories);
         }
 
         fun part2(input: List<String>): Int {
-            TODO("IMPLEMENT")
+            var currentCalories = 0
+            var calorieGroups = mutableListOf<Int>()
+
+            for (line in input) {
+                if (line.isEmpty()) {
+                    // Set and reset
+                    calorieGroups.add(currentCalories)
+                    currentCalories = 0
+                } else {
+                    currentCalories += line.toInt()
+                }
+            }
+
+            // Add last
+            calorieGroups.add(currentCalories)
+
+            return calorieGroups
+                .sorted()
+                .reversed()
+                .take(3)
+                .sum()
         }
     }
 }
--- 2022/src/test/kotlin/Day01Test.kt
+++ 2022/src/test/kotlin/Day01Test.kt
@@ -7,31 +7,27 @@ import org.junit.jupiter.params.provider.CsvSource
 
 internal class Day01Test {
 
-    @ParameterizedTest
-    @CsvSource(
-        "input, 0",
-    )
-    fun `part1 examples`(input: String, expectedResult: Int) {
-        assertEquals(expectedResult, Day01.part1(listOf(input)))
+    @Test
+    fun `part1 example`() {
+        val input = Resource.readFileLines("day01_test")
+        assertEquals(24000, Day01.part1(input))
     }
 
     @Test
     fun `part1 puzzel input`() {
         val input = Resource.readFileLines("day01")
-        assertEquals(0, Day01.part1(input))
+        assertEquals(71502, Day01.part1(input))
     }
 
-    @ParameterizedTest
-    @CsvSource(
-        "input, 0",
-    )
-    fun `part2 examples`(input: String, expectedResult: Int) {
-        assertEquals(expectedResult, Day01.part2(listOf(input)))
+    @Test
+    fun `part2 example`() {
+        val input = Resource.readFileLines("day01_test")
+        assertEquals(45000, Day01.part2(input))
     }
 
     @Test
     fun `part2 puzzel input`() {
         val input = Resource.readFileLines("day01")
-        assertEquals(0, Day01.part2(input))
+        assertEquals(208191, Day01.part2(input))
     }
 }
