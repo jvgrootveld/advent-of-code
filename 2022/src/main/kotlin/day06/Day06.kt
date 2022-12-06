5fac98d55fbbe4b24ddb079edf29f315dc934ba4
Justin van Grootveld <justin.vangrootveld@gmail.com>
2022-12-06 08:02:59 +0100

    Add 2022 day 6

---
+++ 2022/src/main/kotlin/day06/Day06.kt
@@ -0,0 +1,38 @@
+package day06
+
+import java.util.*
+
+class Day06 {
+
+    companion object {
+
+        fun part1(input: String): Int {
+            return findStartOfPacketMarker(input, 4)
+        }
+
+        fun part2(input: String): Int {
+            return findStartOfPacketMarker(input, 14)
+        }
+
+        private fun findStartOfPacketMarker(message: String, markerSize: Int): Int {
+            val messageQueue = ArrayDeque<Char>(markerSize)
+
+            var position = 0
+            for (c in message) {
+
+                if (messageQueue.size == markerSize) {
+                    return position
+                }
+
+                while (messageQueue.contains(c)) {
+                    messageQueue.pop()
+                }
+
+                messageQueue.add(c)
+                position++
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ 2022/src/test/kotlin/Day06Test.kt
@@ -0,0 +1,45 @@
+import day06.Day06
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+internal class Day06Test {
+
+    @ParameterizedTest
+    @CsvSource(
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb, 7",
+        "bvwbjplbgvbhsrlpgdmjqwftvncz, 5",
+        "nppdvjthqldpwncqszvftbrmjlhg, 6",
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg, 10",
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw, 11",
+    )
+    fun `part1 examples`(input: String, expectedResult: Int) {
+        assertEquals(expectedResult, Day06.part1(input))
+    }
+
+    @Test
+    fun `part1 puzzel input`() {
+        val input = Resource.readFile("day06")
+        assertEquals(1080, Day06.part1(input))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb, 19",
+        "bvwbjplbgvbhsrlpgdmjqwftvncz, 23",
+        "nppdvjthqldpwncqszvftbrmjlhg, 23",
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg, 29",
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw, 26",
+    )
+    fun `part2 examples`(input: String, expectedResult: Int) {
+        assertEquals(expectedResult, Day06.part2(input))
+    }
+
+    @Test
+    fun `part2 puzzel input`() {
+        val input = Resource.readFile("day06")
+        assertEquals(3645, Day06.part2(input))
+    }
+}
